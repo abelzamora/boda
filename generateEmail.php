f64f6f8305dd304f25c1ecaa6090fd6a28ea8371
abelfernandez <abelfernandez@192.168.2.107>
2015-09-15 14:17:08 +0200

    generate Email and index.html

--- generateEmail.php
+++ generateEmail.php
@@ -1,3 +1,37 @@
+<?php 
+
+	function getCode(){
+		include('config.php');
+		if(!empty($_GET['id'])){
+			$query_str = "SELECT code FROM ".$dbname." WHERE ID = {$_GET['id']}";
+			$query = mysql_query($query_str) or die(mysql_error());
+			$data = mysql_fetch_array($query);
+			
+			return $data['code'];
+			
+		}
+	}
+	
+	function getName(){
+		include('config.php');
+		if(!empty($_GET['id'])){
+			$query_str = "SELECT name FROM ".$dbname." WHERE ID = {$_GET['id']}";
+			$query = mysql_query($query_str) or die(mysql_error());
+			$data = mysql_fetch_array($query);
+			
+			$str = "";
+			if (strpos($data['name'],' y ') !== false) {
+				$str = "Queridos ".$data['name'].",<br>";
+			}else{
+				$str = "Querido ".$data['name'].",<br>";
+			}
+			
+			return $str;
+				
+		}
+	}
+
+?>
 <html>
 	<head>
 	<style> 
@@ -8,20 +42,29 @@
 		
 		div {
 		   font-family: myFirstFont;
+		   font-size: xx-large;
+		}
+		body{
+			background-image: url("img/wb.jpg");
+			background-repeat: no-repeat;
 		}
 		</style>
 	</head>
 	<body>
 		<div align="center">
+		<br>
+		<?php echo getName();?>
 		Se necesitan invitados con o sin experiencia para bodorrio porque...<br>
 		Nos Casamos!!!!<br>
 		Y como testigos nuestros que sois no podeis faltar.<br><br>
 		
 		Todos los detalles del bodorrio estan en la pagina web que hemos hecho.<br>
 		
-		Para entrar, teneis que utilizar el codigo unico que aparece en la parte inferior de este correo.
+		Para entrar, teneis que utilizar este codigo unico: <?php echo getCode();?>.
 		<br>Por favor, confirmad vuestra asistencia antes del XX.<br><br>
 		NO FALTEIS!!!<br>
+		
+		<a href="http://abelymariacom.ipage.com/login.php">Enlace a la boda</a><br><br>
 		
 		Muchos besos y abrazos.<br>
 		Abel y Maria.
